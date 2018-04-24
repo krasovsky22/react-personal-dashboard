f455992b88666dbe80378f716ef9737e094684e8
vlad <vlad_krasovsky@yahoo.com>
2018-04-23 22:13:24 -0700

    Profile Image step 2

--- src/securedContent/profile/ProfileComponent.jsx
+++ src/securedContent/profile/ProfileComponent.jsx
@@ -19,24 +19,49 @@ class ProfileComponent extends Component {
     const { errors } = this.props
     console.log(this.props, errors)
     return (
-      <Col xl={8} md={8}>
-        <Card>
-          <CardBody>
-            <h3>Profile</h3>
-            <form onSubmit={this.props.handleSubmit(this.handleFormSubmit)}>
-              <Row>
-                <Col xl={6} md={6}>
-                  <Field type="text" name="first_name" error="" label="First Name" component={InputTextField} />
-                </Col>
-                <Col xl={6} md={6}>
-                  <Field type="text" name="last_name" label="Last Name" component={InputTextField} />
-                </Col>
-              </Row>
-              <SubmitInputRight />
-            </form>
-          </CardBody>
-        </Card>
-      </Col>
+      <Row>
+        <Col xl={8} md={8}>
+          <Card>
+            <CardBody>
+              <h3>Profile</h3>
+              <form onSubmit={this.props.handleSubmit(this.handleFormSubmit)}>
+                <Row>
+                  <Col xl={6} md={6}>
+                    <Field type="text" name="first_name" error="" label="First Name" placeholder="First Name" component={InputTextField} />
+                  </Col>
+                  <Col xl={6} md={6}>
+                    <Field type="text" name="last_name" label="Last Name" placeholder="Last Name" component={InputTextField} />
+                  </Col>
+                </Row>
+                <Row>
+                  <Col xl={12} md={12}>
+                    <Field type="text" name="address" error="" label="Address" placeholder="Address" component={InputTextField} />
+                  </Col>
+                </Row>
+                <Row>
+                  <Col xl={4} md={4}>
+                    <Field type="text" name="city" error="" label="City" placeholder="City" component={InputTextField} />
+                  </Col>
+                  <Col xl={4} md={4}>
+                    <Field type="text" name="country" error="" label="Country" placeholder="Country" component={InputTextField} />
+                  </Col>
+                  <Col xl={4} md={4}>
+                    <Field type="text" name="zipcode" error="" label="Postal Code" placeholder="Postal Code" component={InputTextField} />
+                  </Col>
+                </Row>
+                <SubmitInputRight />
+              </form>
+            </CardBody>
+          </Card>
+        </Col>
+
+        <Col xl={4} md={4}>
+          <Card>
+            <CardHeader>Header</CardHeader>
+            <CardBody>Body</CardBody>
+          </Card>
+        </Col>
+      </Row>
     )
   }
 }
